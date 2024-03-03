1e4568138c95ae433dbb10212005b19045827d13
fengandzhy <fengandzhy@gmail.com>
2024-03-03 19:02:38 +1300

    init sql file

---
+++ voting/src/main/resources/dbscript/init.sql
@@ -0,0 +1,6 @@
+INSERT INTO `t_role` VALUES (1, 'ROLE_user', 'Eligible voting general user');
+INSERT INTO `t_role` VALUES (2, 'ROLE_admin', 'Administrator with permissions to vote and view voting results');
+
+
+INSERT INTO `t_candidate` VALUES (1, 'Candidate_A is from UnitA','Candidate_A', 0);
+INSERT INTO `t_candidate` VALUES (2, 'Candidate_B is from UnitB','Candidate_B', 0);
