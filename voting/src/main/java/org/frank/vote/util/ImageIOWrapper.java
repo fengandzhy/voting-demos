5a68747efa5adf16ad4b7abbf9b0663b2f532422
fengandzhy <fengandzhy@gmail.com>
2024-03-22 09:17:26 +1300

    remove everything

--- voting/src/main/java/org/frank/vote/util/ImageIOWrapper.java
+++
@@ -1,9 +0,0 @@
-package org.frank.vote.util;
-
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.OutputStream;
-
-public interface ImageIOWrapper {
-    void writeImage(BufferedImage image, String formatName, OutputStream output) throws IOException;
-}
